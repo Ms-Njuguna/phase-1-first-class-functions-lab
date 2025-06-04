50c1d34b38ef22864ed4799b131857365d68b786
Patricia <patricianjuguna084@gmail.com>
2025-06-04 19:02:57 +0300

    learned about first class functions

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+//created an object
+const drivers = [
+    'Antonia', 
+    'Nuru', 
+    'Amari', 
+    'Mo'
+]
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier;;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3) ;
+
+// Function that selects different drivers using the passed-in function
+function selectDifferentDrivers(arrayOfDrivers, driverFunction) {
+    return driverFunction(arrayOfDrivers);
+}
+
+
+// Temporary logs to test functionality
+console.log("First two drivers:", returnFirstTwoDrivers(drivers));
+console.log("Last two drivers:", returnLastTwoDrivers(drivers));
+console.log("Double 10:", fareDoubler(10));
+console.log("Triple 10:", fareTripler(10));
+console.log("Using selectDifferentDrivers (first):", selectDifferentDrivers(drivers, returnFirstTwoDrivers));
+console.log("Using selectDifferentDrivers (last):", selectDifferentDrivers(drivers, returnLastTwoDrivers));
+
